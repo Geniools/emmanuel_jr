045387792fa0559500d02f4a7ca702af589d4461
liafernando <ameli.masewge.fernando@student.nhlstenden.com>
2022-06-07 22:06:32 +0200

    More Changes

--- website/users/urls.py
+++ website/users/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+
+from . import views
 from .views import index
 
 
 urlpatterns = [
 	path('', index, name='index_user'),
+	path('feedback/', views.feedback),
 ]
--- website/users/views.py
+++ website/users/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import users
+from users.models import Feedback, User
+
+
+def create_user_table(request):
+    User.objects.create(user_id=100001, username="ricka", email="yuanruike2002@outlook.com", password="Keke1234",
+                        birth_date="2012-05-06")
+    User.objects.create(user_id=100002, username="rickb", email="keketty@163.com", password="Keke1234",
+                        birth_date="2002-05-06")
+    User.objects.create(user_id=100003, username="rickc", email="rake8848@sina.cn", password="Keke1234",
+                        birth_date="2002-05-06")
+    User.objects.create(user_id=100004, username="rickd", email="ruike.yuan@student.nhlstenden.com",
+                        password="Keke1234", birth_date="2002-05-06")
+    User.objects.create(user_id=22221111, username="rickd", email="ruike.yuan@student.nhlstenden.com",
+                        password="Keke1234", birth_date="2002-05-06")
+    return HttpResponse("success")
+
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the main index USER PAGE.")
+    return HttpResponse("Hello, world. You're at the main index USER PAGE.")
 
+def feedback(request):
+    return render(request, 'users/feedback.html')
+
+def create_feedback_table(request):
+    Feedback.objects.create(feedback_id=10, topic=50, content="my review is the best because i did it", date="2022-06-06", user_id_id=505050)
+def index(request):
+    if request.method == 'POST':
+        var = request.POST['fname']
