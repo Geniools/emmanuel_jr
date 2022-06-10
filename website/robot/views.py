c565e3d73fc48d58c9c7e956dc4cecb1459454bd
kaiseraftab <kaiser.aftab@student.nhlstenden.com>
2022-06-10 03:16:47 +0200

    some changes

--- website/robot/views.py
+++ website/robot/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.db.models import Q
+from .models import Room
 
 def robot(request):
+    room = Room.objects.all()
+    if request.method =='POST':
+        search = request.POST.get('search')
+        print(search,'==============')
+
+
+
+
+
+
     return render(request, 'robot.html')
