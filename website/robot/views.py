af1b466807ea9b6a077f5345f671f063d5e5022a
kaiseraftab <kaiser.aftab@student.nhlstenden.com>
2022-06-14 09:58:39 +0200

    some changes

--- website/robot/views.py
+++ website/robot/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from robot.models import Room
 
 # Create your views here.
+def robot(request):
+    if request.method == 'POST':
+        if 'roomnumber' in request.POST.keys():
+            controlRobot = request.POST['roomnumber']
+            if Room.objects.filter(room_id=request.POST['roomnumber']).exists():
+                # return render(req, 'robot/liveStream.html',{'message': "directing to room"})
+                success = 'directing to room' + request.POST['roomnumber'] + '...'
+                return HttpResponse(success)
+            else:
+                # return render(req, 'robot/liveStream.html',{'message': "input does not match any data in the database"})
+                success = 'input does not match any data in the database'
+                return HttpResponse(success)
