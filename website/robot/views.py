9e9ace35222a59d63392fb50b5ac14e6d402aaa6
kaiseraftab <kaiser.aftab@student.nhlstenden.com>
2022-06-14 12:49:29 +0200

    some changes

--- website/robot/views.py
+++ website/robot/views.py
@@ -7,12 +7,13 @@ from .models import Room
 def robot(request):
         if request.method == 'POST':
             if 'room' in request.POST.keys():
-                room = request.POST.get('room')
-            if Room.objects.filter(room_id=request.POST.get('room')).exists():
-                redirecting = 'selected room is ' + request.POST.get('room') 
-                return HttpResponse(redirecting)
-            if Room.objects.filter(room_id=request.POST.get('room')).none:
-                failed = 'there is no room that matches with the database'
-                return HttpResponse(failed)
-            
+                if request.POST.get('room')=="":
+                    invalid="You can not leave it blank"
+                    return HttpResponse(invalid)
+                if Room.objects.filter(room_id=request.POST.get('room')).exists():
+                    redirecting = 'selected room is ' + request.POST.get('room') 
+                    return HttpResponse(redirecting)
+                if Room.objects.filter(room_id=request.POST.get('room')).none:
+                    failed = 'there is no room that matches with the database'
+                    return HttpResponse(failed)
         return render(request, 'robot.html')
