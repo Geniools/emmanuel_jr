31f57f7a075a997ab2c1c4c21f8974996113f68b
kaiseraftab <kaiser.aftab@student.nhlstenden.com>
2022-06-14 12:26:51 +0200

    some changes

--- website/robot/views.py
+++ website/robot/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from robot.models import Room
-
+from urllib import request
+from django.shortcuts import render, HttpResponse
+# from .models import Profile
 # Create your views here.
+from .models import Room
+
 def robot(request):
-    if request.method == 'POST':
-        if 'roomnumber' in request.POST.keys():
-            controlRobot = request.POST['roomnumber']
-            if Room.objects.filter(room_id=request.POST['roomnumber']).exists():
-                # return render(req, 'robot/liveStream.html',{'message': "directing to room"})
-                success = 'directing to room' + request.POST['roomnumber'] + '...'
-                return HttpResponse(success)
-            else:
-                # return render(req, 'robot/liveStream.html',{'message': "input does not match any data in the database"})
-                success = 'input does not match any data in the database'
-                return HttpResponse(success)
+        if request.method == 'POST':
+            if 'room' in request.POST.keys():
+                room = request.POST.get('room')
+            if Room.objects.filter(room_id=request.POST.get('room')).exists():
+                redirecting = 'selected room is ' + request.POST.get('room') 
+                return HttpResponse(redirecting)
+            if Room.objects.filter(room_id=request.POST.get('room')).none:
+                failed = 'there is no room that matches with the database'
+                return HttpResponse(failed)
+            
+        return render(request, 'robot.html')
