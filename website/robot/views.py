0600e6d20988c976576b9a4f60db45151e6a64bc
kaiseraftab <kaiser.aftab@student.nhlstenden.com>
2022-06-14 19:52:24 +0200

    changes

--- website/robot/views.py
+++ website/robot/views.py
@@ -11,13 +11,17 @@ def robot(request):
                 if request.POST.get('room')=="":
                     invalid="Please enter room number and you can not leave the field blank"
                     return render(request,'robot.html',{'invalid':invalid})
+
                 elif request.POST.get('room').isnumeric()==False:
                     invalidInput="Please enter valid room number"
                     return render(request,'robot.html',{'invalid':invalidInput})
+                    
                 elif Room.objects.filter(room_id=request.POST.get('room')).exists():
                     redirecting = 'selected room is ' + request.POST.get('room') 
                     return render(request,'robot.html',{'redirecting':redirecting})
+
                 elif Room.objects.filter(room_id=request.POST.get('room')).exists()==False:
                     failed = 'there is no room that matches with the database'
                     return render(request,'robot.html',{'failed':failed})
+                    
         return render(request, 'robot.html')
